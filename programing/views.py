7a46ccc6f09d6daa78d8d0d88344d22555778ba5
ALEX <alexsolastar@gmail.com>
2023-09-11 10:43:53 +0300

    Настройки для Хероку

--- programing/views.py
+++ programing/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-#34545453
 
 
 def page1(request):
--- programing/wsgi.py
+++ programing/wsgi.py
@@ -14,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'programing.settings')
 
 application = get_wsgi_application()
-#5654664536543645643
